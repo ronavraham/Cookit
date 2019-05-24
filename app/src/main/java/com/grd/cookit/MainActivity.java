17fd87bb9cf624187334e1c112417c338864d004
ronavraham <roniii3207@cs.colman.ac.il>
2019-05-24 19:58:47 +0300

    ADD FIREBASE SUPPORT AND GOOGLE LOG IN!!!!

--- app/src/main/java/com/grd/cookit/MainActivity.java
+++ app/src/main/java/com/grd/cookit/MainActivity.java
@@ -3,11 +3,40 @@ package com.grd.cookit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Button;
+
+import com.grd.cookit.activities.AuthActivity;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SignInButton signInBtn;
+    private Button signOutBtn;
+    private final static String TAG = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() == null) {
+            Intent intent = new Intent(this, AuthActivity.class);
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "YAYYY");
+        }
     }
 }
