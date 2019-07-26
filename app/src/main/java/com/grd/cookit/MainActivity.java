a3048b244f845e90c689297e8356d41486570c30
Unknown <golkiv@gmail.com>
2019-07-26 14:32:58 +0300

    added maps to project. replaced all android imports with androidx

--- app/src/main/java/com/grd/cookit/MainActivity.java
+++ app/src/main/java/com/grd/cookit/MainActivity.java
@@ -1,21 +1,16 @@
 package com.grd.cookit;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Button;
 
-import com.grd.cookit.activities.AuthActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.io.IOException;
+import com.grd.cookit.activities.AuthActivity;
 
 public class MainActivity extends AppCompatActivity {
 
