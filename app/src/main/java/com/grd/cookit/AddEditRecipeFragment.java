2848cb2a7dd08ff7be9cf2aceb3e52d14912b6fb
dinzone <dinamitdana1@gmail.com>
2019-07-27 13:43:14 +0300

    attach home fragment to add/edit recipe fragment

--- app/src/main/java/com/grd/cookit/AddEditRecipeFragment.java
+++ app/src/main/java/com/grd/cookit/AddEditRecipeFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -19,7 +20,12 @@ public class AddEditRecipeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // change toolbar title
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.add_recipe_title);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_recipe, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_edit_recipe, container, false);
+        return v;
     }
 }
