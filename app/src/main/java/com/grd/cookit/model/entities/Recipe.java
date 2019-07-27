fe0122966e9ea9213e4ca0b7e62bbdf497d225e0
dinzone <dinamitdana1@gmail.com>
2019-07-27 17:25:45 +0300

    add Recipe java class

---
+++ app/src/main/java/com/grd/cookit/model/entities/Recipe.java
@@ -0,0 +1,51 @@
+package com.grd.cookit.model.entities;
+
+public class Recipe {
+    public String uid;
+    public String name;
+    public String description;
+    public String imageUri;
+    public String userUid;
+    public Long timestamp;
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getContent() {
+        return description;
+    }
+
+    public void setContent(String content) {
+        this.description = content;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getUserUid() {
+        return userUid;
+    }
+
+    public void setUserUid(String userUid) {
+        this.userUid = userUid;
+    }
+}
