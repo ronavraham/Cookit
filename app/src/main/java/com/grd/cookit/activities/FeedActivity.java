c8f3783cc9845c29ce1eec5972882e8c7e79e75a
ronavraham <roniii3207@cs.colman.ac.il>
2019-08-02 19:11:00 +0300

    remove drawer

--- app/src/main/java/com/grd/cookit/activities/FeedActivity.java
+++ app/src/main/java/com/grd/cookit/activities/FeedActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
 import com.grd.cookit.R;
-import com.grd.cookit.drawer.DrawerUtils;
 import com.grd.cookit.fragments.FeedFragment;
 
 import java.io.IOException;
@@ -19,11 +18,6 @@ public class FeedActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.feed_actionbar_title);
         setSupportActionBar(toolbar);
-        try {
-            DrawerUtils.getDrawer(this, toolbar);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         androidx.fragment.app.FragmentManager fragmentManager = getSupportFragmentManager();
         androidx.fragment.app.FragmentTransaction fragmentTransaction =
                 fragmentManager.beginTransaction();
